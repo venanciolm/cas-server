fa44513b9ef9c861b488c9fde022bb24bc5ae871
venanciolm <vlopezm@farmafene.com>
2016-07-14 17:45:53 +0200

    Clean code

--- cas-integration/src/main/java/com/farmafene/cas/integration/ProxyTicketServer.java
+++ cas-integration/src/main/java/com/farmafene/cas/integration/ProxyTicketServer.java
@@ -32,7 +32,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.jasig.cas.authentication.principal.PersonDirectoryPrincipalResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
